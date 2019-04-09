7569bf8050299e37a13f92d4e01f839ad252d471
wojtek <wojtek.kostanski@gmail.com>
2019-04-09 11:47:39 +0200

    add article controller

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe ArticlesController do
+  describe '#index' do
+    subject { get :index }
+
+    it 'should return success response' do
+      subject
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return proper json' do
+      articles = create_list :article, 2
+      subject
+      articles.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq(
+          'title' => article.title,
+          'content' => article.content,
+          'slug' => article.slug
+        )
+      end
+    end
+  end
+end
